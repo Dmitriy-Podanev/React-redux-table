1e4b845ba958c96eb10fd309fb649c7abb42ce54
PodanevDmitriy <podaneovd@mail.ru>
2021-03-29 22:31:37 +0300

    fix v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import {Header} from "./components/Header/Header";
 import axios from "axios";
 import {Table} from "./components/Table/Table";
-
-
+import {useDispatch, useSelector} from "react-redux";
 
 export function App() {
 
- axios.get("http://178.128.196.163:3000/api/records").then(response=>console.log(data = response.data));
+ axios.get("http://178.128.196.163:3000/api/records").then(response=>data=response.data);
 
-
+const dispatch = useDispatch()
+const data = useSelector(state => state.data)
 
   return (
     <div className="App">
       <Header/>
-      <Table data ={data}></Table>
+      <Table/>
     </div>
   );
 }
