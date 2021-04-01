ebb249bff510bb551531bc51ec19795493a83003
PodanevDmitriy <podaneovd@mail.ru>
2021-04-01 23:45:47 +0300

    fixx

--- src/store/userFetch.ts
+++ src/store/userFetch.ts
@@ -11,7 +11,7 @@ export const userFetch = () = >{
     return async (dispatch:Dispatch<AppState.userAction.getUsers>) => {
         try{
            const response  = await axios.get("http://178.128.196.163:3000/api/records");
-           dispatch(appGetUsers) 
+           dispatch() 
         }
         catch(e){}
     }
